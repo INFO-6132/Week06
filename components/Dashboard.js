772582719c1341ecccebae6dea8d039a7901446c
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2022-10-22 18:41:28 -0400

    Signup

---
+++ components/Dashboard.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+import { 
+  Text, 
+  View, 
+  StyleSheet,  
+  TextInput,
+  Button,
+  Alert,
+  ActivityIndicator
+ } from "react-native";
+
+const Dashboard = () => {
+  return (
+    <View><Text>Dashboard</Text></View>
+  )
+}
+
+export default Dashboard;
---
+++ components/Login.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+import { 
+  Text, 
+  View, 
+  StyleSheet,  
+  TextInput,
+  Button,
+  Alert,
+  ActivityIndicator
+ } from "react-native";
+
+const Login = () => {
+  return (
+    <View><Text>Login</Text></View>
+  )
+}
+
+export default Login;

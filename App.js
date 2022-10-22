fef0d4a04500b3d2c8ffd6397c7465586167c989
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2022-10-22 12:28:12 -0400

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
 import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import Login from './components/Login';
-import SignUp from './components/Signup';
-import Dashboard from './components/Dashboard'
 
 import { 
   Text, 
@@ -13,48 +7,12 @@ import {
 
 const Stack = createStackNavigator();
 
-const MyStack = () => {
-  return (
-    <Stack.Navigator
-      initialRouteName="Signup"
-      sreenOptions={{
-        headerTitleAlign: 'center',
-        headerStyle: {
-          backgroundColor: '#3740FE',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={
-            {title: 'Login'}
-          }/>
-        <Stack.Screen
-          name="Signup"
-          component={SignUp}
-          options={
-            {title: 'Signup'}
-          }/>
-        <Stack.Screen
-          name="Dashboard"
-          component={Dashboard}
-          options={
-            {title: 'Dashboard'}
-          }/>
-      </Stack.Navigator>
-  );
-}
-
 const App = () => {
 
   return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
+    <View>
+      <Text>Bare App</Text>
+    </View>
   );
 
 }

0622796b2fa189587d9fb3575d5be2cafb66a648
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2022-10-22 21:03:13 -0400

    Dashboard

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,19 +1,55 @@
 import React, { useState } from 'react';
-
+import {
+  auth, 
+  signOut
+} from "../firebase-config";
 import { 
   Text, 
   View, 
   StyleSheet,  
-  TextInput,
   Button,
-  Alert,
-  ActivityIndicator
  } from "react-native";
 
-const Dashboard = () => {
-  return (
-    <View><Text>Dashboard</Text></View>
-  )
-}
+const Dashboard = ({ navigation }) => {
 
-export default Dashboard;
+  const [uid, setUid] = useState(auth.currentUser.uid);
+  const [displayName, setDisplayName] = useState(auth.currentUser.displayName);
+
+  const logOut = () => {
+    signOut(auth)
+    .then(()=>{
+      navigation.navigate('Login');
+    })
+    .catch((error)=>console.error(error));
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style = {styles.textStyle}>
+        Hello, {displayName}
+      </Text>
+      <Button
+        color="#3740FE"
+        title="Logout"
+        onPress={() => logOut()}
+      />
+    </View>
+  );
+ }
+
+ const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      display: "flex",
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 35,
+      backgroundColor: '#fff'
+    },
+    textStyle: {
+      fontSize: 15,
+      marginBottom: 20
+    }
+  });
+
+ export default Dashboard;
